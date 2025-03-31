1589c940bf600dbf9139d56b71308512c34aacc7
rebe-siro <rebefsilva1808@gmail.com>
2025-03-31 02:30:17 -0400

    Avance del land page

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function mostrarMensaje() {
-    const saludo = document.getElementById('saludo');
-    saludo.textContent = '¡Gracias por visitar mi página!';
+let currentIndex = 0;
+const images = document.querySelectorAll('.slider-image');
+
+function changeImage() {
+    images[currentIndex].style.opacity = 0;
+
+    currentIndex = (currentIndex + 1) % images.length;
+
+    images[currentIndex].style.opacity = 1;
 }
+
+setInterval(changeImage, 5000);
