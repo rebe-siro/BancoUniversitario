48f452704b4cf9b2722f68ed8519b11394a85f23
rebe-siro <rebefsilva1808@gmail.com>
2025-04-18 01:32:13 -0400

    Abrir cuenta listoo by Rebe

--- script.js
+++ script.js
@@ -26,3 +26,33 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
             
+function exportarAExcel() {
+    const form = document.getElementById('formulario');
+    const datos = new FormData(form);
+    const nuevoRegistro = {};
+  
+    for (let [key, value] of datos.entries()) {
+      nuevoRegistro[key] = value;
+    }
+  
+    let registros = JSON.parse(localStorage.getItem("datosEmpresa")) || [];
+  
+    registros.push(nuevoRegistro);
+  
+    localStorage.setItem("datosEmpresa", JSON.stringify(registros));
+  
+    const ws = XLSX.utils.json_to_sheet(registros);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Formulario");
+  
+    XLSX.writeFile(wb, "datos_empresa.xlsx");
+  
+    const mensajeExito = document.getElementById("mensaje-exito");
+    mensajeExito.style.display = "block";
+  
+    setTimeout(() => {
+      mensajeExito.style.display = "none";
+    }, 5000);
+  
+    form.reset();
+  }
